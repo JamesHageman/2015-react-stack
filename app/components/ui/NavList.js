97336915f435893afb7932376960b26d8c643d04
James Hageman <jamesd@hageman.ca>
2015-06-10 11:31:19 -0400

    Added hamburger menu

--- app/components/ui/NavList.js
+++ app/components/ui/NavList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router'
 import styles from '../stylesheets/NavList.css'
 import Box from '../ui/Box'
+import className from 'classnames'
 
 var {arrayOf, shape, string, bool} = React.PropTypes;
 
@@ -16,30 +17,51 @@ var NavList = React.createClass({
     )
   },
 
-  render() {
+  getInitialState() {
+    return {
+      collapse: true
+    };
+  },
+
+  _toggleDropdown() {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  },
+
+  _renderList() {
     var {items} = this.props;
+    return items.map((item) => {
+      var sharedProps = {
+        key: item.title,
+        target: item.target,
+        className: className(styles.link,
+                             this.state.collapse && styles.collapse)
+      };
+
+      var content = (
+        <Box className={styles.linkBox}>
+          {item.title}
+        </Box>
+      );
+
+      if (item.route || item.route === undefined) {
+        return <Link to={item.link} {...sharedProps}>{content}</Link>
+      } else {
+        return <a href={item.link} {...sharedProps}>{content}</a>
+      }
+    })
+  },
+
+  render() {
+
     return (
       <Box padding={false} column>
+        <Box className={styles.dropdownToggle}>
+          <button onClick={this._toggleDropdown}>Hamburger</button>
+        </Box>
         {
-          items.map((item) => {
-            var sharedProps = {
-              key: item.title,
-              target: item.target,
-              className: styles.link
-            };
-
-            var content = (
-              <Box className={styles.linkBox}>
-                {item.title}
-              </Box>
-            );
-
-            if (item.route || item.route === undefined) {
-              return <Link to={item.link} {...sharedProps}>{content}</Link>
-            } else {
-              return <a href={item.link} {...sharedProps}>{content}</a>
-            }
-          })
+          this._renderList()
         }
       </Box>
     )
