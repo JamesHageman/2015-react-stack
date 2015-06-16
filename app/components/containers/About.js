6ef84718f10ed7a278315b73ce4be376615c664f
James Hageman <jamesd@hageman.ca>
2015-06-16 09:58:23 -0400

    About page to bootstrap

---
+++ app/components/containers/About.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ModalExample from '../ui/ModalExample';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="col-md-12">
+        <div className="row">About page</div>
+        <ModalExample/>
+      </div>
+    );
+  }
+}
+
+export default Home;
